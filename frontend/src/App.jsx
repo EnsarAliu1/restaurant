9f435697ae3486a406fd0db53bac2c6ef1e29bcc
Ensar <ensaraliu2017@gmail.com>
2026-06-22 14:52:18 +0200

    feat: update dependencies and integrate Sentry for error tracking
    - Added new dependencies: @sentry/react, @tanstack/react-query, lucide-react, and react-router.
    - Removed unused CSS file (App.css) and assets (hero.png, react.svg, vite.svg).
    - Refactored App component to include PageLoader and Layout components.
    - Created new components: Footer, Layout, Navbar, PageLoader, SentryErrorFallback, and SentryUserSync.
    - Integrated Sentry for error tracking and user context synchronization with Clerk.
    - Updated main entry point to include QueryClientProvider and Sentry error boundary.
    - Configured Vite to use Tailwind CSS.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,32 @@
-import './App.css'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
+import { Show, SignInButton, SignUpButton, useAuth, UserButton } from "@clerk/react";
+import PageLoader from "./components/PageLoader";
+import Layout from "./components/Layout";
 
 function App() {
 
+  const { isLoaded } = useAuth();
+
+  if(!isLoaded) return <PageLoader/>
+
   return (
-    <>
+    <Layout>
       <header>
         <Show when="signed-out">
-          <SignInButton mode='modal'/>
-          <SignUpButton mode='modal'/>
+          <SignInButton mode="modal" />
+          <SignUpButton mode="modal" />
         </Show>
         <Show when="signed-in">
           <UserButton />
         </Show>
       </header>
-    </>
-  )
+
+      <button className="btn btn-primary">Click Me</button>
+      <button className="btn btn-secondary">Click Me</button>
+      <button className="btn btn-outline">Click Me</button>
+
+    </Layout>
+  );
 }
 
-export default App
+export default App;
